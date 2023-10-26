1bbb38ba7bae84a1391854a56fbb2cb0f6b78b2e
Badar-69 <109582705+Badar-69@users.noreply.github.com>
2023-10-26 23:58:30 +0500

    Added firebase config

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { db } from '../../firebase.js'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Keyboard, Pagination, Navigation } from 'swiper/modules';
 import { Link } from 'react-router-dom';
---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLNng41qK5S6B_St3bHqFWfTrq2-4CWHc",
+  authDomain: "testpro-008.firebaseapp.com",
+  databaseURL: "https://testpro-008-default-rtdb.firebaseio.com",
+  projectId: "testpro-008",
+  storageBucket: "testpro-008.appspot.com",
+  messagingSenderId: "204502448872",
+  appId: "1:204502448872:web:f4452c4fe569418ded675d",
+  measurementId: "G-N2WTYBBFCS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//Firestore connection
+const db = getFirestore();
+
+export { db };
+export { app }
